de18eb3a35fded82525da960f914066cad1de668
Yannick Merckx <dockwekerym@gmail.com>
2016-04-22 13:55:15 +0200

    First version of Optimus Prime

--- OptimusPrime.py
+++ OptimusPrime.py
@@ -1,25 +1,49 @@
 import csv
 import sys
+import random
 import xml.etree.ElementTree as ET
+
+#id;Performer;Title;Inst.;Style;Year;Tempo;Number of Notes
 
 training = open(sys.argv[1])
 
 trainingset = csv.reader(training, delimiter=";")
 
+Performers = set()
+Insts = set()
+Styles = set()
+Years = set()
+Tempos = set()
+
+
 for row in trainingset:
     id = row[0]
-    tree = ET.parse(id + ".xml")
-    root = tree.getroot()
+    Performers.add(row[1])
+    Insts.add(row[3])
+    Styles.add(row[4])
+    Years.add(row[5])
+    Tempos.add(row[6])
+    # tree = ET.parse(id + ".xml")
+    # root = tree.getroot()
 
-    #Here is where a part of the magic happens
 
 test = open(sys.argv[2])
 
+
+
 testset = csv.reader(test, delimiter=";")
 
-for row in testset
-    id = row[0]
-    tree = ET.parse(id + ".xml")
-    root = tree.getroot()
 
-    #Here is where the other part of the magic happens
+   
+
+with open(sys.argv[3], 'w') as csvfile:
+    fieldnames = ['id','Performer','Inst','Style','Year','Tempo']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames,  delimiter=";")
+    writer.writeheader()
+    for row in testset:
+        id = row[0]
+        writer.writerow({'id': id ,'Performer': (random.sample(Performers, 1))[0] ,'Inst': (random.sample(Insts, 1))[0],'Style': (random.sample(Styles, 1))[0],'Year': (random.sample(Years, 1))[0],'Tempo' : (random.sample(Tempos, 1))[0]})
+        # tree = ET.parse(id + ".xml")
+        # root = tree.getroot()
+
+
---
+++ crossvalidate.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl
+use List::Util qw(sum);
+
+# Configuration
+$noFolds = 5;
+
+# Command-line parameters
+$inputDir = $ARGV[0];
+
+# Read dataset
+open (my $fh, '<', $inputDir . '/dataset-balanced.csv');
+    my @lines = <$fh>;
+    @lines = @lines[ 1 .. $#lines ]; #skip first line
+close($fh);
+
+
+
+# Cross-validation loop
+for ($fold=0; $fold < $noFolds; $fold++) {
+    # Open training and test file
+    open (my $test_fh,     '>test-data-file-' . $fold .'.csv');
+    open (my $training_fh, '>training-data-file-' . $fold . '.csv');
+
+    # Separate training and test data according to fold
+    $c=0;
+    $tc=0;
+    foreach $line (@lines) {
+        if (($c - $fold) % 5 == 0) {
+            # Test item, strip data to be predicted
+            @fields = split(';', $line);            
+            $strip_line = $fields[0] . ';;;;;;;' . "\n";
+            #$strip_line = $fields[1] . ';' . $fields[3] . ';' . $fields[4] . ';' . $fields[5] . ';' . $fields[6] . "\n";            
+
+            $performer[$tc] = $fields[1];
+            $instrument[$tc] = $fields[3];
+            $style[$tc] = $fields[4];
+            $year[$tc] = $fields[5];
+            $tempo[$tc] = $fields[6];            
+            
+            print $test_fh $strip_line;
+            $tc=$tc+1;
+        }
+        else {
+            # Training item
+            print $training_fh $line;
+        }
+        
+        $c=$c+1;
+    }   
+    
+    # Close files
+    close($test_fh);
+    close($training_fh);
+    
+    # Run classifier    
+    system('python OptimusPrime.py training-data-file-' . $fold .'.csv test-data-file-' .$fold . '.csv output-file-' . $fold . '.csv');
+    
+    # Compare outputs with data that was stripped
+    open (my $fh, '<', 'output-file-' . $fold .'.csv');
+    my @lines = <$fh>;
+       
+    # Keep score
+    $c=0;
+    for $line(@lines) {
+        @field = split(';', $line);
+                
+        $performerPerformance[$fold] += !($field[0] eq $performer[$c]);
+        $instrumentPerformance[$fold] += !($field[1] eq $instrument[$c]);
+        $stylePerformance[$fold] += !($field[2] eq $style[$c]);
+        $yearPerformance[$fold] += abs($field[3] - $year[$c]);
+        $tempoPerformance[$fold] += abs($field[4] - $tempo[$c]);
+        
+        $c=$c+1;
+        
+    }    
+}
+
+
+# Aggregate score
+print "Error performance (lower is better)\n";
+print "-----------------------------------\n";
+print "Performer prediction\t" . join(';', @performerPerformance) . " => " . sum(@performerPerformance) ."\n";
+print "Instrument prediction\t" . join(';', @instrumentPerformance) . " => " . sum(@instrumentPerformance) ."\n";
+print "Style prediction\t" . join(';', @stylePerformance) . " => " . sum(@stylePerformance) ."\n";
+print "Year prediction\t\t" . join(';', @yearPerformance) . " => " . sum(@yearPerformance) ."\n";
+print "Tempo prediction\t" . join(';', @tempoPerformance) . " => " . sum(@tempoPerformance) ."\n";
+
